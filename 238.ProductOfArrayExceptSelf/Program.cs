449d72f7768f88363a460e3355390c2cdc12f8ab
mustafa altuntaş <altun_tas@outlook.com>
2024-11-07 23:21:22 +0300

    .

---
+++ 238.ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+
+var Input1 = new int[] { 1, 2, 3, 4 };
+var Output1 = new int[] { 24, 12, 8, 6 };
+
+var Input2 = new int[] { -1, 1, 0, -3, 3 };
+var Output2 = new int[] { 0, 0, 9, 0, 0 };
+
+Console.WriteLine($"Test1: [{string.Join(',',ProductExceptSelf(Input1))}] == [{string.Join(',', Output1)}] {Output1.SequenceEqual(ProductExceptSelf(Input1))}");
+Console.WriteLine($"Test2: [{string.Join(',',ProductExceptSelf(Input2))}] == [{string.Join(',', Output2)}] {Output2.SequenceEqual(ProductExceptSelf(Input2))}");
+
+
+int[] ProductExceptSelf(int[] nums)
+{
+    int n = nums.Length;
+    var left = new int[n];
+    var right = new int[n];
+    var result = new int[n];
+
+    left[0] = 1;
+    right[n - 1] = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        left[i] = left[i-1]*nums[i-1];
+    }
+    for(int i = n-2; i >= 0; i--)
+    {
+        right[i] = right[i + 1] * nums[i+1];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        result[i] = left[i]*right[i];
+    }
+
+    return result;
+
+}
+
+
+
+/*
+ Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+ */
+
+
+
+
+Console.ReadLine();
+
+
